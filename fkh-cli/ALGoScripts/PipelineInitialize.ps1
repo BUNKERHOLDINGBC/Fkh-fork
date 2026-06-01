49a05881a502cb7019cb461583d58930aa2db9ab
Freddy Kristiansen <fkr@freddy.dk>
2026-06-01 21:38:24 +0200

    transfer settings

--- fkh-cli/ALGoScripts/PipelineInitialize.ps1
+++ fkh-cli/ALGoScripts/PipelineInitialize.ps1
@@ -5,9 +5,6 @@ if ($doNotPublishApps) {
 
 # BcContainerHelper override
 
-$ENV:FKH_BACKEND_URL = "https://fkh-freddydk-backend.azurewebsites.net/api"
-$ENV:FKH_TIMEZONE = "Europe/Copenhagen"
-
 $parts = "$ENV:GITHUB_REPOSITORY". Split('/')
 $containerName = "$($parts[0])-$($parts[1])-$($ENV:_project)-$($ENV:_buildMode)-$($ENV:GITHUB_RUN_ID)".ToLower() -replace "[^a-z0-9\-]"
 
@@ -16,8 +13,38 @@ $container = $containers.containers | Where-Object { $_.appLabel -eq $containerN
 if (-not $container) {
     $adminUsername = "admin"
     $adminPassword = GetRandomPassword
-    Write-Host "Creating container $containerName"
-    $result = fkh createcontainer --name $containerName --artifactUrl $artifact --adminUsername $adminUsername --adminPassword $adminPassword --autostop 6h --useOIDC --asJson --repo $ENV:REPOSITORY --project $ENV:_project
+    $settings = $ENV:settings | ConvertFrom-Json
+
+    $params = @{
+        name           = $containerName
+        artifactUrl    = $artifact
+        adminUsername  = $adminUsername
+        adminPassword  = $adminPassword
+        autostop       = "6h"
+        repo           = $ENV:REPOSITORY
+        project        = $ENV:_project
+    }
+    if ($settings.PSObject.Properties.Name -eq "fkh") {
+        $fkhSettings = $settings."fkh"
+        foreach ($prop in $fkhSettings.PSObject.Properties) {
+            if ($prop.Name -like "CreateContainer.*") {
+                $paramName = $prop.Name.Substring("CreateContainer.".Length)
+                if (-not $params.ContainsKey($paramName)) {
+                    $params[$paramName] = $prop.Value
+                }
+            }
+        }
+    }
+    $fkhArgs = @("createcontainer", "--useOIDC", "--asJson")
+    foreach ($key in $params.Keys) {
+        if ($params[$key]) {
+            $fkhArgs += "--$key"
+            $fkhArgs += "$($params[$key])"
+        }
+    }
+
+    Write-Host "Creating container $containerName $($fkhArgs -join ' ')"
+    $result = fkh @fkhArgs
     Write-Host "CreateContainer Result: $result"
     $container = $result | ConvertFrom-Json
 }
