27f643f1122e800f33ebe56b0d959be2e5b79b3a
Freddy Kristiansen <fkr@freddy.dk>
2026-07-31 06:57:55 +0200

    remove images_subject

--- terraform/identity.tf
+++ terraform/identity.tf
@@ -73,7 +73,7 @@ resource "azurerm_federated_identity_credential" "github_actions" {
 # Additional federated credential using GitHub's immutable ID-form subject.
 # Created only when the numeric IDs are supplied (they are auto-provided by the github.repository_owner_id / github.repository_id context in the deploy workflow).
 resource "azurerm_federated_identity_credential" "github_actions_idform" {
-  count = var.create_images_owner_id != "" && var.create_images_repo_id != "" && var.create_images_subject == "" ? 1 : 0
+  count = var.create_images_owner_id != "" && var.create_images_repo_id != "" ? 1 : 0
 
   name                      = "github-actions-createimages-id"
   user_assigned_identity_id = azurerm_user_assigned_identity.github_actions.id
